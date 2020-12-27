611bf63486270325f65a76bcbf9c7b596e80b09a
Emersson Cardim Mota <63078277+CardimEmersson@users.noreply.github.com>
2020-12-27 15:28:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react'
+import {Formik, Field, Form, ErrorMessage} from 'formik'
+import schema from './schema.js'
 import './App.css';
 
 function App() {
+
+  // function validate(values) {
+  //   const errors = {}
+  //   if(!values.name) {
+  //     errors.name = 'Nome é obrigatorio';
+  //   }
+  //   if(!values.email) {
+  //     errors.email = 'Email é obrigatorio';
+  //   }
+
+  //   return errors;
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik 
+        // validate={validate}
+        validationSchema={schema}
+        initialValues={{
+          name: '',
+          email: '',
+        }}
+        onSubmit={(values, actions) => {
+          console.log('SUBMIT', values)
+        }}
+      >
+        {({ values, errors, touched, isValid }) => (
+          <Form>
+            <div>
+              <label htmlFor="name">Nome</label>
+              {/* <input type="text" name="name" value={values.name} onChange={handleChange}/> */}
+              <Field type="text" name="name" />
+              <ErrorMessage name="name"/>
+              {/* {errors.name && touched.name && (
+                <span>{errors.name}</span>
+              )} */}
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              {/* <input type="email" name="email" value={values.email} onChange={handleChange}/> */}
+              <Field type="email" name="email" />
+              <ErrorMessage name="email"/>
+              {/* {errors.email && touched.email &&  (
+                <span>{errors.email}</span>
+              )} */}
+            </div>
+            <button type="submit" disabled={!isValid}>Enviar</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
